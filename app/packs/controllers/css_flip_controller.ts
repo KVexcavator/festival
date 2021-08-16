0adea2cbe31ca85f15e368198144c02f142a5119
Excavanor <str_token@example.com>
2021-08-16 11:41:31 +0300

    adding animating, transitions css

--- app/packs/controllers/css_flip_controller.ts
+++ app/packs/controllers/css_flip_controller.ts
@@ -1,27 +1,39 @@
 import { Controller } from "stimulus"
 
-export default class CssController extends Controller {
-  static classes = ["css"]
-  cssClass: string
+export default class CssFlipController extends Controller {
+  static classes = ["on", "off"]
+  onClass: string
+  offClass: string
+
   static targets = ["elementToChange"]
   elementToChangeTarget: HTMLElement
+
   static values = { status: Boolean }
   statusValue: boolean
 
   toggle(): void {
     this.flipState()
   }
+
   flipState(): void {
     this.statusValue = !this.statusValue
   }
+
   statusValueChanged(): void {
     this.updateCssClass()
   }
+
   updateCssClass(): void {
-    for (const oneCssClass of this.cssClass.split(" ")) {
+    for (const oneCssClass of this.onClass.split(" ")) {
       this.elementToChangeTarget.classList.toggle(
         oneCssClass,
         this.statusValue
+      )
+    }
+    for (const oneCssClass of this.offClass.split(" ")) {
+      this.elementToChangeTarget.classList.toggle(
+        oneCssClass,
+        !this.statusValue
       )
     }
   }
